fbd835fa6b409cdf4bdd3b5467b1452b7f654dcb
Mohammad Zaid <mdzaid786291@gmail.com>
2024-04-02 22:53:14 +0530

    kdflksjdflkj

--- faceDetection.py
+++ faceDetection.py
@@ -8,4 +8,6 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fronta
 faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1, minNeighbors=5,minSize=(30,30) )
 for (x,y,w,h) in faces:
     cv2.rectangle(img, (x,y),(w+x, h+y),(0,255,0),5)
-
+cv2.imshow("dflk",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
