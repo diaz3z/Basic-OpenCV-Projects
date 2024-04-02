4af2c6849e256e6d808d9382e51961232999cec8
Mohammad Zaid <mdzaid786291@gmail.com>
2024-04-02 22:52:07 +0530

    kdjf

--- faceDetection.py
+++ faceDetection.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("")
+img = cv2.imread("img2.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascade+"haarcascade_frontalface_default.xml")
-faces = face_cascade.detectMultiScale(gray, 1.3, 5, (30,30) )
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1, minNeighbors=5,minSize=(30,30) )
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y),(w+x, h+y),(0,255,0),5)
+
