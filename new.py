45f94ed439caf4fbc541506a83f7d6cff21b7e1c
Mohammad Zaid <mdzaid786291@gmail.com>
2024-04-05 00:07:58 +0530

    dsjfksjdlfkjskl

--- new.py
+++ new.py
@@ -3,7 +3,6 @@ import numpy as np
 
 img = cv2.imread("img2.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1, minNeighbors=5,minSize=(30,30) )
 for (x,y,w,h) in faces:
