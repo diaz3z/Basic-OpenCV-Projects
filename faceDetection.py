1f98eb291f227487a69a8a859a2c49d791445f15
Mohammad Zaid <mdzaid786291@gmail.com>
2024-04-06 00:01:40 +0530

    h

--- faceDetection.py
+++ faceDetection.py
@@ -4,4 +4,9 @@ import numpy as np
 img = cv2.imread("img2.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-face = face_cascade.detectMultiScale(gray, 1.3, 5, (30,30))
+face = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
+for (x,y,w,h) in face:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+cv2.imshow("image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
